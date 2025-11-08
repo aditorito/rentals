7d6335d0334ed986d4f8073ff78b52728781de6f
aditorito <as0742004@gmail.com>
2025-11-08 18:04:06 +0000

    driver fullname reflected

---
+++ rentals/rentals/doctype/drivers/drivers.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2025, BWH and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Drivers(Document):
+	def before_save(self):
+		self.fullname = f"{self.firstname} {self.lastname}"
