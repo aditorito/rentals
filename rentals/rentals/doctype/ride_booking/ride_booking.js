b5b461ca0d98a85722526ae11a51e07d7e6f351a
aditorito <as0742004@gmail.com>
2025-11-20 19:53:01 +0000

    ride booking

---
+++ rentals/rentals/doctype/ride_booking/ride_booking.js
@@ -0,0 +1,39 @@
+// Copyright (c) 2025, BWH and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Ride Booking", {
+    refresh(frm) {
+
+    },
+
+    rate(frm) {
+        // recalculate total
+        frm.trigger("update_total_amount")
+
+    },
+
+    update_total_amount(frm) {
+        let total_di = 0;
+        for (let item of frm.doc.items) {
+            total_di += item.distance;
+
+        }
+        const amount = frm.doc.rate * total_di
+        frm.set_value("total_amount", amount)
+    }
+});
+
+frappe.ui.form.on("Ride Booking Item", {
+    refresh(frm) {
+
+    },
+    distance(frm, cdt, cdn) {
+        frm.trigger("update_total_amount")
+
+    },
+    items_remove(frm) {
+        frm.trigger("update_total_amount")
+
+
+    }
+});
