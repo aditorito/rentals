075963788242731fdc48da796035a9782511a653
aditorito <as0742004@gmail.com>
2026-01-02 17:42:36 +0000

    added ride booking doctype

---
+++ rentals/rentals/doctype/ride_order/ride_order.js
@@ -0,0 +1,20 @@
+// Copyright (c) 2025, BWH and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Ride Order", {
+    refresh(frm) {
+        if (frm.doc.status === "New") {
+            frm.add_custom_button("Accept", () => {
+                frm.set_value("status", "Accepted");
+                frm.save()
+            },"Action")
+            frm.add_custom_button("Rejected", ()=>{
+                frm.set_value("status", "Rejected");
+                frm.save()
+            },"Action")
+
+        }
+
+
+    },
+});
