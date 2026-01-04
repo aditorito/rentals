faf422033bdad237eb95db863048026b081a857e
aditorito <as0742004@gmail.com>
2026-01-04 17:51:55 +0000

    wrote a test case for my drivers

--- rentals/rentals/doctype/drivers/test_drivers.py
+++ rentals/rentals/doctype/drivers/test_drivers.py
@@ -1,9 +1,17 @@
 # Copyright (c) 2025, BWH and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestDrivers(FrappeTestCase):
-	pass
+	def test_full_name_correctly_set(self):
+		test_driver = frappe.new_doc("Drivers")
+		test_driver.firstname = "John"
+		test_driver.lastname = "Doe"
+		test_driver.licensenumber = "HP32US1234"
+		test_driver.phone_number = "9792282411"
+		test_driver.save()
+
+		self.assertEqual(test_driver.fullname, "John Doe")
