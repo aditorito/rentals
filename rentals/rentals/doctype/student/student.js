275e01d96fd3a066f2e5ed290853fc0e046e2810
aditorito <as0742004@gmail.com>
2026-01-11 18:35:08 +0000

    added logic of percentage

---
+++ rentals/rentals/doctype/student/student.js
@@ -0,0 +1,27 @@
+// Copyright (c) 2026, BWH and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Student", {
+	refresh(frm) {
+        
+
+	},
+    validate(frm){
+        let arr = frm.doc.marksheet
+        let total_marks = 0;
+        for (let i = 0; i < arr.length; i++) {
+            total_marks = total_marks +  arr[i].marks
+            
+            
+        }
+        console.log(total_marks);
+        
+
+        let percentage = total_marks/arr.length;
+
+        frm.set_value('percentage', percentage)
+        
+
+    }
+});
+
