d6e6221d968e4d5585819b58e5673d4ccf6f6709
aditorito <as0742004@gmail.com>
2026-01-12 17:57:52 +0000

    marks edit setting

--- rentals/rentals/doctype/student/student.js
+++ rentals/rentals/doctype/student/student.js
@@ -2,24 +2,35 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Student", {
-	refresh(frm) {
-        
+    refresh(frm) {
 
-	},
-    validate(frm){
+
+    },
+    validate(frm) {
         let arr = frm.doc.marksheet
         let total_marks = 0;
         for (let i = 0; i < arr.length; i++) {
-            total_marks = total_marks +  arr[i].marks
-            
-            
-        }
-        
+            total_marks = total_marks + arr[i].marks
 
-        let percentage = total_marks/arr.length;
+
+        }
+
+
+        let percentage = arr.length ? total_marks / arr.length : 0;
+
 
         frm.set_value('percentage', percentage)
-        
+
+        if (frm.doc.status != 'Active') {
+
+            frm.fields_dict["marksheet"].grid.update_docfield_property(
+                "marks",
+                "read_only",
+                1
+            );
+
+        }
+
 
     }
 });
