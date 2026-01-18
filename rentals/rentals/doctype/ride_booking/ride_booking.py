6083d90f83799bd400e75a7a7ce158c1db51ab52
aditorito <as0742004@gmail.com>
2026-01-18 18:13:44 +0000

    improvment

--- rentals/rentals/doctype/ride_booking/ride_booking.py
+++ rentals/rentals/doctype/ride_booking/ride_booking.py
@@ -7,11 +7,12 @@ from frappe.model.document import Document
 
 class RideBooking(Document):
 	def validate(self):
-		
+
 		if not self.rate:
 			self.rate = frappe.db.get_single_value("Rentals Settings", "standard_rate")
 
 		total_distance = 0
+		
 		for item in self.items:
 			total_distance += item.distance
 
